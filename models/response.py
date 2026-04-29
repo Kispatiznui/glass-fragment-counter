5e71ddaa79f7e76386c9abf53e0f18c06fee730f
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-28 22:49:23 -0500

    Update response.py

--- models/response.py
+++ models/response.py
@@ -1,1 +1,17 @@
+def build_response(result):
 
+    sizes = result["sizes"]
+
+    if len(sizes) > 0:
+        avg = sum(sizes) / len(sizes)
+        mx = max(sizes)
+        mn = min(sizes)
+    else:
+        avg = mx = mn = 0
+
+    return {
+        "fragmentos": result["count"],
+        "promedio": avg,
+        "maximo": mx,
+        "minimo": mn
+    }
