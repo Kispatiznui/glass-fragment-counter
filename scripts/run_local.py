71d6d77da8989b9116753ba7fa6e8f4220be0062
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-28 22:50:19 -0500

    Update run_local.py

--- scripts/run_local.py
+++ scripts/run_local.py
@@ -1,1 +1,11 @@
+import time
+import cv2
+from app.processor import process_image
 
+img = cv2.imread("data/input/test.jpg")
+
+start = time.time()
+process_image(img)
+end = time.time()
+
+print("Tiempo:", end - start)
